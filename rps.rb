64ef3522a74cf3374d983bb5f2f925911fe6e503
Ned Bless <ebless@lsoc.org>
2016-03-10 11:02:44 -0600

    Added game class

--- rps.rb
+++ rps.rb
@@ -5,9 +5,17 @@ class Player
 		@name = gets.chomp
 		@wins = 0
 		@losses = 0
-		play_rps
 	end
-	def play_rps
+	
+
+end
+class Game
+	attr_accessor :user
+	def initialize(user)
+		@user = user
+		play_rps(user)
+	end
+	def play_rps(user)
 		puts "What do you choose (r for rock, p for paper, s for scissors)?"
 		@choice = gets.chomp
 		
@@ -17,56 +25,57 @@ class Player
 				case computer_choice
 				when "r"
 					puts "Tie!"
-					self.play_rps
+					self.play_rps(user)
 				when "p"
 					puts "Computer wins!"
-					@losses = @losses + 1
-					puts "Your all time wins and losses are: #{@wins} wins and #{@losses} losses."
-					self.play_rps
+					user.losses = user.losses + 1
+					puts "Your all time wins and losses are: #{user.wins} wins and #{user.losses} losses."
+					self.play_rps(user)
 				when "s" 
 					puts "You win!"
-					@wins = @wins + 1
-					puts "Your all time wins and losses are: #{@wins} wins and #{@losses} losses."
-					self.play_rps
+					user.wins = user.wins + 1
+					puts "Your all time wins and losses are: #{user.wins} wins and #{user.losses} losses."
+					self.play_rps(user)
 				end
 		when "p"
 				case computer_choice
 				when "r"
 					puts "You win!"
-					@wins = @wins + 1
-					puts "Your all time wins and losses are: #{@wins} wins and #{@losses} losses."
-					self.play_rps
+					user.wins = user.wins + 1
+					puts "Your all time wins and losses are: #{user.wins} wins and #{user.losses} losses."
+					self.play_rps(user)
 				when "p"
 					puts "Tie!"
-					self.play_rps
+					self.play_rps(user)
 				when "s"
 					puts "Computer wins!"
-					@losses = @losses + 1
-					puts "Your all time wins and losses are: #{@wins} wins and #{@losses} losses."
-					self.play_rps
+					user.losses = user.losses + 1
+					puts "Your all time wins and losses are: #{user.wins} wins and #{user.losses} losses."
+					self.play_rps(user)
 				end
 		when "s"
 				case computer_choice
 				when "r"
 					puts "Computer wins!"
-					@losses = @losses + 1
-					puts "Your all time wins and losses are: #{@wins} wins and #{@losses} losses."
-					self.play_rps
+					user.losses = user.losses + 1
+					puts "Your all time wins and losses are: #{user.wins} wins and #{user.losses} losses."
+					self.play_rps(user)
 				when "p"
 					puts "You win!"
-					@wins = @wins + 1
-					puts "Your all time wins and losses are: #{@wins} wins and #{@losses} losses."
-					self.play_rps
+					user.wins = user.wins + 1
+					puts "Your all time wins and losses are: #{user.wins} wins and #{user.losses} losses."
+					self.play_rps(user)
 				when "s"
 					puts "Tie!"
-					self.play_rps
+					self.play_rps(user)
 				end
 		else
 			puts "Sorry, you didn't choose correctly!"
-			self.play_rps
+			self.play_rps(user)
 		end
 	end
-
 end
 
-Player.new
+player = Player.new
+game = Game.new(player)
+game.play_rps(player)
